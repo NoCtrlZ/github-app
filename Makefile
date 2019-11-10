8c640862c1cdec728daf70cab2cbffcefa4d6008
Hemlocked <phantomofrotten@gmail.com>
2019-11-10 21:35:18 +0900

    create server and build script

---
+++ Makefile
@@ -0,0 +1,4 @@
+rust: rust-client/src/main.rs
+	cd rust-client && cargo run src/main.rs
+ruby: ruby-client/template_server.rb
+	cd ruby-client && ruby template_server.rb
--- rust-client/src/main.rs
+++ rust-client/src/main.rs
@@ -1,3 +1,17 @@
+use std::net::TcpListener;
+
+fn instance_listen(port: &str) {
+    let listener = TcpListener::bind(format!("localhost{}", port)).unwrap();
+
+    for stream in listener.incoming() {
+
+        let stream = stream.unwrap();
+
+        println!("{:?}", stream);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let port = ":3000";
+    instance_listen(port);
 }
